b440a71e1b92d657cddab1a16d833b001de3fa68
Dennis Murphy <dennismurphyjr33@gmail.com>
2019-12-07 17:19:30 -0500

    added checkout page

--- src/reducer.js
+++ src/reducer.js
@@ -11,8 +11,34 @@ export default (state = INITIAL_STATE, action) => {
             cart.push(action.payload);
             return {
                 ...state,
-                cart
+                cart,
             };
+        }
+        case actions.WIPE_CART: {
+            const cart = [...INITIAL_STATE.cart];
+            return {
+                ...state,
+                cart,
+            }
+        }
+        case actions.REMOVE_FROM_CART: {
+            const cart = [...state.cart];
+            let searching = true;
+            let i = 0;
+            while(searching) {
+                if(i === action.payload) {
+                    cart.splice(i, 1);
+                    searching = false;
+                }
+            i++;
+            if (i >= cart.length) {
+                searching = false
+            }
+            }
+            return {
+                ...state,
+                cart,
+            }
         }
         default: {
             return state;
